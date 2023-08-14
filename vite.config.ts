99c4950144ac3d1ff353ddbc377f2510a5e18337
孙晓飞 <411223871@qq.com>
2023-08-14 17:18:34 +0800

    fix：路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: "public",
+  base: "./", // 解决服务器访问静态资源报错
 });
